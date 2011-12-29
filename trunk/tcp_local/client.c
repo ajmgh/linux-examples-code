9e9beded2c9c883043306e626b6fbb4a95ab0f50
42600102@qq.com <42600102@qq.com@472439c0-a053-6023-6c91-8a1d4a15cd09>
2011-12-29 15:09:44 +0000

    双向改造

--- trunk/tcp_local/client.c
+++ trunk/tcp_local/client.c
@@ -3,40 +3,63 @@
 #include <sys/socket.h>
 #include <sys/un.h>   
 
-#define POWER_STANDBY_CONTRL_DOMAIN "/tmp/power_standby_control"   
+#define BUFSIZE 8192
+#define INOTIFY_INTERACTIVE_DOMAIN "/tmp/inotify_interactive_domain"   
+#define SENDSTR "ybz21_test_client"
 
 int main(int argc,char *argv[])
 {
-   	int connect_fd;
-    	int ret;
-    	char snd_buf[1024];
-    	int i;
-    	static struct sockaddr_un srv_addr;
+	int connect_fd;
+	int ret;
+	char buf[BUFSIZE];
+	int i, len;
+	static struct sockaddr_un pin;
 
 	//creat unix socket
-    	connect_fd=socket(PF_UNIX,SOCK_STREAM,0);
-    	if(connect_fd<0)
-    	{
-        	perror("cannot create communication socket");
-        	return 1;
-    	}   
-    	srv_addr.sun_family=AF_UNIX;
-    	strcpy(srv_addr.sun_path, POWER_STANDBY_CONTRL_DOMAIN);
+	connect_fd=socket(PF_UNIX,SOCK_STREAM,0);
+	if(connect_fd<0)
+	{
+		perror("cannot create communication socket");
+		return -1;
+	}   
+	pin.sun_family=AF_UNIX;
+	strcpy(pin.sun_path, INOTIFY_INTERACTIVE_DOMAIN);
 	//connect server
-    	ret=connect(connect_fd,(struct sockaddr*)&srv_addr,sizeof(srv_addr));
-    	if(ret==-1)
-    	{
-        	perror("cannot connect to the server");
-        	close(connect_fd);
-        	return 1;
-    	}
-    	memset(snd_buf,0,1024);
+	ret=connect(connect_fd,(struct sockaddr*)&pin,sizeof(pin));
+	if(ret==-1)
+	{
+		perror("cannot connect to the server");
+		close(connect_fd);
+		return -1;
+	}
+
+	memset(buf,0,BUFSIZE);
+	strcpy(buf, SENDSTR);
+	printf("Sending message %s to server ...\n", buf);
+
+	if(send(connect_fd, buf, sizeof(buf), 0) == -1)
+	{
+		perror("Error in send\n");
+		return -1;
+	}
+
+	printf("..sent message.. wait for response...\n");
+
+	while(1)
+	{
+		memset(buf, 0x00, BUFSIZE);
+		if(recv(connect_fd, buf, BUFSIZE, 0) == -1)
+		{
+			perror("Error in receiving response from server\n");
+			return -1;
+		}
 		
-    	strcpy(snd_buf,"CMD_GOTO_STANDBY");
-
-	//send info server
-      	write(connect_fd,snd_buf, sizeof(snd_buf));
-
+		len = strlen(buf);
+		printf("\nResponse form server buf len = %d\n", len);
+		printf("\nResponse form server: \n%s\n", buf);
+		//usleep(10000);
+		sleep(1);
+	}
 	close(connect_fd);
-    	return 0;	
+	return 0;	
 }
